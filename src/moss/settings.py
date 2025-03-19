64176fc6ba913f73d46c7d41056912b540f54eb6
Michael A. Smith <michael@smith-li.com>
2025-03-19 11:41:27 -0400

    feat(settings): connect to aws

--- src/moss/settings.py
+++ src/moss/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 # TODO: use a 12-factor friendly configuration tool such as environ-config
+import os
 from collections.abc import Sequence
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "moss.store.apps.StoreConfig",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,38 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "store.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+}
+
+AWS_STORAGE_BUCKET_NAME = "unmoving_stone"
+##
+# Don't put our secret AWS credentials in here
+# Instead, let the boto3 default credential chain handle them
+# For the PoC I'll set up a profile for it. IRL we'd probably
+# want to use the ec2 instance profile or whatever profile
+# is sensible for the runtime container.
+# TODO: Make this configurable
+# HACK: don't mess with the environment
+os.environ["AWS_PROFILE"] = AWS_PROFILE = "moss"
+
+
+# TODO: this should go in django checks
+def _check_aws_conn():
+    """quick check to ensure we have a valid aws connection"""
+    import pprint
+
+    import boto3
+
+    sts = boto3.client("sts")
+    aws_user = sts.get_caller_identity()
+    pprint.pprint(aws_user)
+
+
+_check_aws_conn()
